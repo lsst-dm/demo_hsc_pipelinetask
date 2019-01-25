68fe66d85f0c6fc7765b8e80585422309cfe7c5d
Yusra AlSayyad <yusra@astro.princeton.edu>
2019-01-24 21:25:20 -0600

    Add coaddition configs
    Copied and expanded from obs_subaru unless otherwise
    noted in inline comments

---
+++ config/compareWarpAssembleCoadd.py
@@ -0,0 +1,10 @@
+config.matchingKernelSize = 29
+config.doSigmaClip = False
+config.subregionSize = (10000, 200)  # 200 rows (since patch width is typically < 10k pixels)
+config.doMaskBrightObjects = False  #  Different from obs_subaru
+config.removeMaskPlanes.append("CROSSTALK")
+config.doNImage = True
+config.badMaskPlanes += ["SUSPECT"]
+config.assembleStaticSkyModel.subregionSize = (10000, 200)
+from lsst.pipe.tasks.selectImages import PsfWcsSelectImagesTask
+config.select.retarget(PsfWcsSelectImagesTask)
